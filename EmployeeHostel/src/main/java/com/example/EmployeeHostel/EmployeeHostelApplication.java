c8cedabf0bc0329388884cbdbb2f70549e799180
sayalikurh <sayalikurh@sayalikurh-wi10.cybage.com>
2022-04-04 15:56:46 +0530

    rest template

---
+++ EmployeeHostel/src/main/java/com/example/EmployeeHostel/EmployeeHostelApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeHostel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeHostelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeHostelApplication.class, args);
+	}
+
+}
---
+++ EmployeeHostel/src/test/java/com/example/EmployeeHostel/EmployeeHostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeHostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeHostelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
